c9f383437f25958194340c27423b0dd5f1009058
sherwynds <w7s1b@ugrad.cs.ubc.ca>
2018-10-13 00:58:08 -0700

    Added java implementation of InfoPanel.

---
+++ editor/app/src/main/gen/ca/ubc/eml/soiltopargraphy/editor/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package ca.ubc.eml.soiltopargraphy.editor;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
