aab34e16bd29fd3d6e49538e509fd022e3e76cbd
Ranky Lau <rankylau@gmail.com>
2018-10-27 00:29:27 -0700

    created layout for ImagePanel, added method to add picture from the gallery or choose from the camera. Also, added required permission to AndroidManifest

---
+++ editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/ui/infopanel/ImagePanelViewModel.kt
@@ -0,0 +1,8 @@
+package ca.ubc.eml.soiltopargraphy.editor.ui.infopanel
+
+import android.arch.lifecycle.ViewModel
+import android.net.Uri
+
+class ImagePanelViewModel : ViewModel(){
+    var uri: Uri?=null
+}// TODO: Implement the ViewModel
