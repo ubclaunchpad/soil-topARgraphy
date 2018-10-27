df9484040df13793190a7779ba01afbdabe7dac1
Ranky Lau <rankylau@gmail.com>
2018-10-27 01:42:55 -0700

    Successfully used ViewModel to pass data around

--- editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/ui/infopanel/InfoPanelFragment.kt
+++ editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/ui/infopanel/InfoPanelFragment.kt
@@ -11,6 +11,7 @@ import android.widget.ImageView
 import android.widget.TextView
 
 import ca.ubc.eml.soiltopargraphy.editor.R
+import org.w3c.dom.Text
 
 class InfoPanelFragment : Fragment() {
 
@@ -23,11 +24,21 @@ class InfoPanelFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view  = inflater.inflate(R.layout.info_panel_fragment, container, false)
-        //find all three field
+        //find all four field
+        val descModel = activity?.run {
+            ViewModelProviders.of(this).get(DescriptionPanelViewModel::class.java)
+        }
         val titleView = view.findViewById<TextView>(R.id.Info_panel_title)
         val descriptionView = view.findViewById<TextView>(R.id.Info_panel_description)
-        val ImageView = view.findViewById<ImageView>(R.id.ImageView)
-        val
+        val imageView = view.findViewById<ImageView>(R.id.ImageView)
+        val imageTitleView = view.findViewById<TextView>(R.id.Image_Title)
+        val imgmodel = activity?.run{
+            ViewModelProviders.of(this).get(ImagePanelViewModel::class.java)
+        }
+        titleView.text = descModel?.name
+        descriptionView.text = descModel?.description
+        imageView.setImageURI(imgmodel?.uri)
+        imageTitleView.text = imgmodel?.imageTitle
         return view
     }
 
