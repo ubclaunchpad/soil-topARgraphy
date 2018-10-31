99bddb379a7215385f6bb52e31dbfd80e7647f2c
Sarah Bornais <Sarah@Sarahs-MacBook-Pro-2.local>
2018-10-31 16:37:40 -0700

    added default toolbar that just has an option to view the list of flags

--- editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/ui/flag/FlagMapFragment.java
+++ editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/ui/flag/FlagMapFragment.java
@@ -5,7 +5,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -22,7 +29,16 @@ public class FlagMapFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.flag_map_fragment, container, false);
+        View view = inflater.inflate(R.layout.flag_map_fragment, container, false);
+        Toolbar flagToolBar = view.findViewById(R.id.flagToolBar);
+
+        //Sets the action bar for the activity as the custom flag toolbar
+        ((AppCompatActivity)getActivity()).setSupportActionBar(flagToolBar);
+
+        //Ensures that the menu is actually displayed in the toolbar
+        setHasOptionsMenu(true);
+
+        return view;
     }
 
     @Override
@@ -30,6 +46,30 @@ public class FlagMapFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(FlagMapViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_flag_not_selected, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        //Using a switch here so that eventually, when we add more options in the toolbar, this function can handle multiple possible selections
+        switch (item.getItemId()) {
+            //When the "List View" button is selected, swaps this fragment out for the flag list fragment
+            case R.id.action_to_listview:
+                FragmentManager manager = getActivity().getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.container, new FlagListFragment());
+                transaction.commit();
+                break;
+            default:
+                break;
+        }
+
+        return true;
     }
 
 }
