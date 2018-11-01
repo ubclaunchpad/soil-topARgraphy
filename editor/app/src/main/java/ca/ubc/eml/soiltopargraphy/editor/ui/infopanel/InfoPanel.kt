04d777fb5f0a32b3f52b85daaa277cc96ea9f831
Daphne Liu <daphnel15113455@gmail.com>
2018-10-31 22:06:07 -0700

    Updated RecyclerView version

--- editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/ui/infopanel/InfoPanel.kt
+++ editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/ui/infopanel/InfoPanel.kt
@@ -1,10 +1,11 @@
 package ca.ubc.eml.soiltopargraphy.editor.ui.infopanel
 
+import android.net.Uri
 import ca.ubc.eml.soiltopargraphy.editor.ui.quizpanel.QuizPanel
 
 data class InfoPanel(
         val name: String,
         val description: String,
-        val image: Any, // TODO decide on type
+        val image: Uri, // TODO decide on type
         val quiz: QuizPanel
 )
