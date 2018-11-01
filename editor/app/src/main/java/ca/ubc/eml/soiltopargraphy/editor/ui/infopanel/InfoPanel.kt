310f70001e9ae64e2cdec0733dfb8b482b84787c
Ranky Lau <rankylau@gmail.com>
2018-10-31 22:39:03 -0700

    Fixed image rotation problem

--- editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/ui/infopanel/InfoPanel.kt
+++ editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/ui/infopanel/InfoPanel.kt
@@ -3,8 +3,8 @@ package ca.ubc.eml.soiltopargraphy.editor.ui.infopanel
 import ca.ubc.eml.soiltopargraphy.editor.ui.quizpanel.QuizPanel
 
 data class InfoPanel(
-        val name: String,
-        val description: String,
-        val image: Any, // TODO decide on type
-        val quiz: QuizPanel
+val name: String,
+val description: String,
+val image: Any, // TODO decide on type
+val quiz: QuizPanel
 )
