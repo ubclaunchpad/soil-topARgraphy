e0a51dec2e91e41a9d109b8bd095e5e02257b031
Ranky Lau <rankylau@gmail.com>
2018-11-02 00:07:31 -0700

    Fixed issue with picture capture from camera

--- editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/ui/infopanel/ImagePanelFragment.kt
+++ editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/ui/infopanel/ImagePanelFragment.kt
@@ -3,10 +3,17 @@ package ca.ubc.eml.soiltopargraphy.editor.ui.infopanel
 import android.Manifest
 import android.app.Activity.RESULT_OK
 import android.arch.lifecycle.ViewModelProviders
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
@@ -19,6 +26,25 @@ import android.widget.EditText
 import android.widget.ImageView
 import ca.ubc.eml.soiltopargraphy.editor.R
 import com.bumptech.glide.Glide
+import android.system.Os.mkdir
+import java.nio.file.Files.exists
+import java.io.File.separator
+import android.os.Environment.getExternalStorageDirectory
+import android.os.StrictMode
+import android.support.annotation.RequiresApi
+import android.support.v4.content.FileProvider
+import android.util.Log
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.net.URI
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 /**
@@ -27,28 +53,29 @@ import com.bumptech.glide.Glide
 
 
 class ImagePanelFragment : Fragment() {
-
+    lateinit var photoPath: String
+    lateinit var photoUri: Uri
     private var mViewModel: ImagePanelViewModel? = null
     private lateinit var mImageView: ImageView
 
     fun onImageAddCameraButtonClick() {
         //check permission
         var camerapermission = ContextCompat.checkSelfPermission(this.context!!, Manifest.permission.CAMERA)
-        var writeExternalPermission = ContextCompat.checkSelfPermission(this.context!!,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        var writeExternalPermission = ContextCompat.checkSelfPermission(this.context!!, Manifest.permission.WRITE_EXTERNAL_STORAGE)
         val requestCode = 0
         if (camerapermission != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this.requireActivity(),
                     arrayOf(Manifest.permission.CAMERA),
                     requestCode)
         }
-        if(writeExternalPermission != PackageManager.PERMISSION_GRANTED){
+        if (writeExternalPermission != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this.requireActivity(),
                     arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
                     requestCode)
         }
-        camerapermission = ContextCompat.checkSelfPermission(this.context!!,Manifest.permission.CAMERA)
-        writeExternalPermission = ContextCompat.checkSelfPermission(this.context!!,Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        if (camerapermission == PackageManager.PERMISSION_GRANTED && writeExternalPermission== PackageManager.PERMISSION_GRANTED)
+        camerapermission = ContextCompat.checkSelfPermission(this.context!!, Manifest.permission.CAMERA)
+        writeExternalPermission = ContextCompat.checkSelfPermission(this.context!!, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (camerapermission == PackageManager.PERMISSION_GRANTED && writeExternalPermission == PackageManager.PERMISSION_GRANTED)
             addImageFromCamera()
 
     }
@@ -74,16 +101,42 @@ class ImagePanelFragment : Fragment() {
         startActivityForResult(getIntent, IMAGE_PICK_CODE)
     }
 
+    fun createImageFile(): File? {
+        val fileName = "MyPicture"
+        val storageDir = context?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+                fileName,
+                ".jpg",
+                storageDir
+        )
+        photoPath = image.absolutePath
+        return image
+    }
+
     fun addImageFromCamera() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        val getIntent = Intent(Intent.ACTION_PICK)
-        intent.putExtra(Intent.EXTRA_INITIAL_INTENTS,getIntent)
-        startActivityForResult(Intent.createChooser(intent, "Pick an appropriate app"), CAMERA_PICK_CODE)
+        if(intent.resolveActivity(context?.packageManager)!=null){
+            var photoFile: File? = null
+            try{
+                photoFile = createImageFile()
+            }
+            catch (e: IOException){}
+            if(photoFile != null){
+                 photoUri = FileProvider.getUriForFile(
+                        context!!,
+                        "ca.ubc.eml.soiltopargraphy.editor.fileProvider",
+                        photoFile
+                )
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri)
+                startActivityForResult(intent, CAMERA_PICK_CODE)
+            }
+        }
     }
+
     fun onToViewInfoPanelButtonClick() {
         mViewModel?.imageTitle = view?.findViewById<EditText>(R.id.image_title)?.text.toString()
         val manager = activity?.supportFragmentManager
-        if(manager!=null){
+        if (manager != null) {
             val transaction = manager.beginTransaction()
             transaction.replace(R.id.container, InfoPanelFragment.newInstance())
             transaction.addToBackStack(null)
@@ -100,14 +153,11 @@ class ImagePanelFragment : Fragment() {
         val addImageViaGalleryButton = view.findViewById<Button>(R.id.add_image_via_gallery_button)
         addImageViaGalleryButton.setOnClickListener { onImageAddGalleryButtonClick() }
         val toViewInfoPanelButton = view.findViewById<Button>(R.id.to_view_info_panel_button)
-        toViewInfoPanelButton.setOnClickListener {onToViewInfoPanelButtonClick() }
+        toViewInfoPanelButton.setOnClickListener { onToViewInfoPanelButtonClick() }
+        mImageView = view.findViewById(R.id.imageView)
         return view
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        mImageView = view.findViewById(R.id.imageView)
-    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -130,10 +180,10 @@ class ImagePanelFragment : Fragment() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
         if (requestCode == CAMERA_PICK_CODE && resultCode == RESULT_OK) {
-            val imageBitmap = data.extras.get("data") as Bitmap
-            Glide.with(context!!).load(data.data).into(mImageView)
-            view?.findViewById<ImageView>(R.id.imageView)?.visibility = View.VISIBLE
-            mViewModel?.uri = data.data
+            mViewModel?.uri = photoUri
+            mImageView.setImageURI(mViewModel?.uri)
+            Glide.with(context!!).load(photoUri).into(mImageView)
+            mImageView?.visibility = View.VISIBLE
         }
         if (requestCode == IMAGE_PICK_CODE && resultCode == RESULT_OK) {
 
