1054746ee7ce92d03afb17a0f7572196a3d82af9
Sarah Bornais <Sarah@Sarahs-MacBook-Pro-2.local>
2018-11-02 14:04:36 -0700

    added code from main FlagMapViewModel to resolve conflicts

--- editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/ui/flag/FlagMapViewModel.java
+++ editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/ui/flag/FlagMapViewModel.java
@@ -1,9 +1,15 @@
 package ca.ubc.eml.soiltopargraphy.editor.ui.flag;
 
 import android.arch.lifecycle.ViewModel;
+import java.util.List;
+import ca.ubc.eml.soiltopargraphy.editor.db.AppRepository;
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
 
-public class FlagMapViewModel extends ViewModel {
+public class FlagMapViewModel extends AndroidViewModel {
     private boolean flagItemClicked;
+    private AppRepository mRepository;
 
     public void setFlagItemClicked(boolean isClicked) {
         flagItemClicked = isClicked;
@@ -12,4 +18,14 @@ public class FlagMapViewModel extends ViewModel {
     public boolean getFlagItemClicked() {
         return flagItemClicked;
     }
+
+    // gets a reference to the repository and he list of words from the repository.
+    public FlagMapViewModel (Application application) {
+        super(application);               // needs AndroidViewModel
+        mRepository = new AppRepository(application);
+    }
+
+    LiveData<List<Flag>> getFlags() { return mRepository.getAllFlags(); }
+
+    public void insert(Flag flag) { mRepository.insertFlag(flag); }
 }
