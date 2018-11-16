8595ae8c89507e3e6d7fb9b2a8fa907080cb36e0
Ranky Lau <rankylau@gmail.com>
2018-11-15 17:55:35 -0800

    rebased with the master branch, and added button to save info panel, right now I have removed quizPanel assiocation with it since it has not been implemented in the master branch

--- editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/ui/infopanel/InfoPanel.kt
+++ editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/ui/infopanel/InfoPanel.kt
@@ -3,13 +3,11 @@ package ca.ubc.eml.soiltopargraphy.editor.ui.infopanel
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 import android.net.Uri
-import ca.ubc.eml.soiltopargraphy.editor.ui.quizpanel.QuizPanel
 
 @Entity(tableName = "info_panel_table")
 data class InfoPanel(
         @PrimaryKey
         val name: String,
         val description: String,
-        val image: Uri, // TODO decide on type
-        val quiz: QuizPanel
+        val image: Uri
 )
