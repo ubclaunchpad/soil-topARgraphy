9463632632211cc004ebf7bff02a3ebbd2be0a28
Sarah Bornais <Sarah@Sarahs-MacBook-Pro-2.local>
2018-11-15 18:50:03 -0800

    added functions to get flags asscoiated with a specific terrain

--- editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/db/FlagDao.java
+++ editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/db/FlagDao.java
@@ -23,4 +23,17 @@ public interface FlagDao {
 
     @Query("DELETE FROM flag_table WHERE id = :flagId")
     void deleteFlag(int flagId);
+
+    // Given a certain terrainId, gets all the flags in the table associated with that terrainId
+    // and stores in a LiveData list. This should be used when getting flags to be displayed in UI
+    // where LiveData is useful
+    @Query("SELECT * from flag_table WHERE terrainId = :terrainId")
+    LiveData<List<Flag>> getFlagsInTerrainLive(int terrainId);
+
+    // Given a certain terrainId, gets all the flags in the table associated with that terrainId
+    // but does NOT store it in a LiveData list. This is used to get the flags that should be deleted
+    // when a terrain is deleted (a function that doesn't require LiveData).
+    @Query("SELECT * from flag_table WHERE terrainId = :terrainId")
+    List<Flag> getFlagsInTerrainNotLive(int terrainId);
+
 }
