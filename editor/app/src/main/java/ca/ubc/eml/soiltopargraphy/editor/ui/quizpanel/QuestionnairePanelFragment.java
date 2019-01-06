9568053c1735f64436055ea93a7e55bcd985ccb6
SophieMBerger <berger.m.sophie@gmail.com>
2019-01-06 15:47:47 -0800

    Questionnaire to main

--- editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/ui/quizpanel/QuestionnairePanelFragment.java
+++ editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/ui/quizpanel/QuestionnairePanelFragment.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import ca.ubc.eml.soiltopargraphy.editor.R;
+import ca.ubc.eml.soiltopargraphy.editor.ui.flag.FlagMapFragment;
 
 /**
  */
@@ -71,4 +74,13 @@ public class QuestionnairePanelFragment extends Fragment {
         QuestionnairePanel newSoilType = new QuestionnairePanel(soilType, color, density, fruitfulness, humidity);
     }
 
-}
+
+    public void onClick(View v) {
+        Fragment nextFragment = new FlagMapFragment();
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        android.support.v4.app.FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, nextFragment).commit();
+    }
+    }
+
+
