84e4a00f220bda4af38b0c0cff59faef1207ed75
Sophie <berger.m.sophie@gmail.com>
2019-03-21 21:05:52 -0700

    Android Share Target implimentation started

--- editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/MainActivity.kt
+++ editor/app/src/main/java/ca/ubc/eml/soiltopargraphy/editor/MainActivity.kt
@@ -1,5 +1,6 @@
 package ca.ubc.eml.soiltopargraphy.editor
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import ca.ubc.eml.soiltopargraphy.editor.ui.main.MainFragment
@@ -20,6 +21,22 @@ class MainActivity : AppCompatActivity(){
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+
+        when {
+            intent?.action == Intent.ACTION_SEND -> {
+                if ("text/plain" == intent.type) {
+                    handleSendText(intent) // Handle text being sent
+                } 
+            }
+        }
+
+    }
+
+    private fun handleSendText(intent: Intent){
+        intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
+            //Update UI to reflect text being shared
+        }
+
     }
 
 }
